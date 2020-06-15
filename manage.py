f42c2edea7e0c77df069419f90a9b1c625c73d17
YoungHaYoo12 <youngha.sparky@gmail.com>
2020-06-15 13:26:24 +0000

    Make shell context for app and db.

--- manage.py
+++ manage.py
@@ -1,7 +1,8 @@
 import os
-from flask_script import Manager
+from flask_script import Manager,Shell
 from flask_migrate import Migrate
 from app import create_app, db
+from app.models import Hospital,User,Patient,PatientNote,Treatment,Appointment
 
 app = create_app('default')
 manager = Manager(app)
@@ -37,6 +38,18 @@ def test(coverage=False):
     COV.html_report(directory=covdir)
     print('HTML version: file://%s/index.html'%covdir)
     COV.erase()
+
+# shell context for app
+def make_shell_context():
+  return dict(app=app,
+              db=db,
+              Hospital=Hospital,
+              User=User,
+              Patient=Patient,
+              PatientNote=PatientNote,
+              Appointment=Appointment,
+              Treatment=Treatment)
+manager.add_command("shell", Shell(make_context=make_shell_context))
 
 if __name__ == '__main__':
   manager.run()
