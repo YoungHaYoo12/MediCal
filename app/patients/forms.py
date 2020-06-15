fd45cdcfb67bba12c1038d2f50bd9fa25a4fdbe2
YoungHaYoo12 <youngha.sparky@gmail.com>
2020-06-15 10:04:18 +0000

    Add 'patients' list,add,delete view.

---
+++ app/patients/forms.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import Email, DataRequired, Length
+
+class PatientAddForm(FlaskForm):
+  first_name = StringField('First Name',validators=[DataRequired(message='First Name Field Empty')])
+  last_name = StringField('Last Name',validators=[DataRequired(message='Last Name Field Empty')])
+  email = StringField('Email',validators=[DataRequired(message='Email Field Empty'),Email(message='Email Format Not Correct'),Length(1,64)])
+  submit = SubmitField('Add')
--- app/patients/views.py
+++ app/patients/views.py
@@ -1,6 +1,54 @@
-from flask import render_template
+from flask import render_template,request,redirect,flash,url_for,abort
+from flask_login import login_required,current_user
+from app import db
+from app.models import Personnel, Patient
 from app.patients import patients
+from app.patients.forms import PatientAddForm
 
-@patients.route('/')
-def index():
-  return "Patients"
+@patients.route('/personnel_list/<email>')
+@login_required
+def list(email):
+  page = request.args.get('page',1,type=int)
+  
+  # retrieve personnel
+  personnel = Personnel.query.filter_by(email=email).first_or_404()
+
+  # retrieve patients of personnel
+  query = personnel.patients.order_by(Patient.last_name.asc())
+  pagination = query.paginate(page,per_page=10)
+  patients = pagination.items
+
+  return render_template('patients/list.html',patients=patients,pagination=pagination)
+
+@patients.route('/add',methods=['GET','POST'])
+@login_required
+def add():
+  form = PatientAddForm()
+
+  if form.validate_on_submit():
+    # create new patient
+    patient = Patient(first_name=form.first_name.data,
+                      last_name=form.last_name.data,
+                      email=form.email.data)
+    current_user.patients.append(patient)
+    db.session.add(patient)
+    db.session.commit()
+
+    flash('New Patient Added')
+    return redirect(url_for('patients.list',email=current_user.email))
+  
+  return render_template('patients/add.html',form=form)
+
+@patients.route('/delete/<int:id>')
+@login_required
+def delete(id):
+  # confirm that patient is connected to current_user
+  patient = Patient.query.get_or_404(id)
+  if not patient in current_user.patients.all():
+    abort(403)
+
+  db.session.delete(patient)
+  db.session.commit()
+
+  flash('Patient Succesfully Deleted')
+  return redirect(url_for('patients.list',email=current_user.email))
