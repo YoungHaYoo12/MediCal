c949111484d9487311adca84331726043ec53aed
YoungHaYoo12 <youngha.sparky@gmail.com>
2020-06-15 11:11:39 +0000

    Add username field and username validator.

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -14,6 +14,7 @@ class LoginForm(FlaskForm):
 class RegistrationForm(FlaskForm):
   first_name = StringField('First Name', validators=[DataRequired(message='First Name Field Empty')])
   last_name = StringField('First Name', validators=[DataRequired(message='Last Name Field Empty')])  
+  username = StringField('Username', validators=[DataRequired(message='Username Field Empty')])
   email = StringField('Email', validators=[DataRequired(message="Email Field Empty"), 
           Length(1,64), Email(message="Email Format Not Correct")])
   password = PasswordField('Password',validators=[DataRequired(message='Password Field Empty'), 
@@ -21,6 +22,10 @@ class RegistrationForm(FlaskForm):
   password2 = PasswordField('Confirm Password',validators=[DataRequired('Confirm Password Field Empty')])
   submit = SubmitField('Register')
 
+  def validate_username(self,field):
+    if User.query.filter_by(username=field.data).first():
+      raise ValidationError('Username Already Taken')
+
   def validate_email(self,field):
-    if Personnel.query.filter_by(email=field.data).first():
+    if User.query.filter_by(email=field.data).first():
       raise ValidationError('Email Already Registered')
