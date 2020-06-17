15e74573f52865e81009f8278284312b85603e85
YoungHaYoo12 <youngha.sparky@gmail.com>
2020-06-17 10:10:23 +0000

    Change url parameter name from id to patient_id.

--- app/patient_notes/views.py
+++ app/patient_notes/views.py
@@ -3,11 +3,11 @@ from flask_login import login_required
 from app.patient_notes import patient_notes
 from app.models import Patient
 
-@patient_notes.route('/patient/<int:id>')
+@patient_notes.route('/patient/<int:patient_id>')
 @login_required
 def list(id):
   # retrive patient_notes if patient exists
-  patient = Patient.query.get_or_404(id)
+  patient = Patient.query.get_or_404(patient_id)
   patient_notes = patient.patient_notes.all()
 
   return render_template('patient_notes/list.html',
