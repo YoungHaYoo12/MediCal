769fa31a5fc65fd9eb7cc1bab6def6c4642ac0ec
YoungHaYoo12 <youngha.sparky@gmail.com>
2020-06-18 09:24:48 +0000

    Set up flask-moment.

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,10 @@ from config import config
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_moment import Moment
 
 db = SQLAlchemy()
+moment = Moment()
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
@@ -15,6 +17,7 @@ def create_app(config_name):
 
   # Initialize Flask Extension Instances 
   db.init_app(app)
+  moment.init_app(app)
   login_manager.init_app(app)
 
   # Register blueprints
