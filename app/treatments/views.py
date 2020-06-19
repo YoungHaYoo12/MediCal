2488d5b4710b9284ec6ef0494e7cf706d98ce2d8
YoungHaYoo12 <youngha.sparky@gmail.com>
2020-06-19 06:32:07 +0000

    Adapt treatments.add view function.
    Adapt view function to the implementation of the many-to-many field
    between 'Treatment' and 'Hospital' models.

--- app/treatments/views.py
+++ app/treatments/views.py
@@ -22,8 +22,21 @@ def add():
   form = TreatmentForm()
 
   if form.validate_on_submit():
-    treatment = Treatment(name=form.name.data)
-    treatment.hospital = current_user.hospital
+    hospital = current_user.hospital
+
+    # create new treatment if it does not already exist
+    treatment = Treatment.query.filter_by(name=form.name.data).first()
+    if treatment is None:
+      treatment = Treatment(name=form.name.data)
+    
+    # if hospital already has treatment, redirect
+    if hospital in treatment.hospitals.all():
+      flash('Hospital Already Has Treatment')
+      return redirect(url_for('treatments.list'))
+
+    # add treatment to hospital
+    treatment.hospitals.append(hospital)
+
     db.session.add(treatment)
     db.session.commit()
     flash('Treatment Successfully Added')
