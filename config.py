48822296b34b7cd7223a1a24c074738983372ce3
YoungHaYoo12 <youngha.sparky@gmail.com>
2020-10-21 17:38:27 +0900

    update production config settings.

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
 import os
+from dotenv import find_dotenv, load_dotenv
+
 basedir = os.path.abspath(os.path.dirname(__file__))
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 
 class Config():
   SECRET_KEY = os.environ.get('SECRET_KEY')
